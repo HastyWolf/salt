26c1df562e14824d5a396eada248852ab59ec136
hastywolf <wolfmanchu@gmail.com>
2021-07-12 20:00:29 -0400

    Python notes

---
+++ chris-dev-server/Python.py
@@ -0,0 +1,36 @@
+print("I never drink after midnight")
+    print("But ")
+
+# Variables
+# What is a variable. They are holding containers to store a value.
+# They can store different types. *They cannot start with a number*
+
+age = 5
+print("age")
+
+# Algorithms
+# Set of instructions for the computer, that is followed in a sequence.
+
+
+# -----------------------------------------------------------------------
+
+
+
+# Input fuction
+# captured information from a user that can be used as a variable.
+
+person = input("Enter your name:")
+print("Hello",person)
+
+# Concatenation with the Input function
+x = input("Enter a number: ")
+y = input("Enter another number: ")
+z = x + y
+print(z)
+# This will print the numbers as a 'String' or text
+
+x = input("Enter a number: ")
+y = input("Enter another number: ")
+z = print(int(x) +int(y))
+# The following converts the value as an intiger
+
